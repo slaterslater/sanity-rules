885f0752dadd296ad85f0541bbe3cdd0366926de
slaterslater <slaterslater@gmail.com>
2021-10-27 09:58:25 -0400

    adds optional max param

--- rules.js
+++ rules.js
@@ -1,12 +1,13 @@
 import { api } from './sanity.json'
 
-export const maxSize = async image => {
+// validation for maximum allowable image filesize
+// image param is passed from Rule.custom() and optional max param is integer of KB  
+export const maxSize = async (image, max = 100) => {
   if (!image) return true
   const [type, id, dim, ext] = image.asset._ref.split('-')
   const imageURL = `https://cdn.sanity.io/images/${api.projectId}/${api.dataset}/${id}-${dim}.${ext}?q=100`
   const res = await fetch(imageURL, {method: 'HEAD'})
-  const size = Number(res.headers.get("content-length"))
-  const max = 100 * 1000 // 100KB
-  console.log(type, size/1000, 'KB')
-  return size <= max || `current image is ${Math.ceil(size/1000)}KB and must not exceed 100KB` 
+  const size = Number(res.headers.get("content-length")) / 1000
+  console.log(type, size, 'KB')
+  return size <= max || `current image is ${Math.ceil(size)}KB and must not exceed ${max}KB` 
 }
