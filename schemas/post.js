0321b5515edf0358266426efaed72506ac0b8926
slaterslater <slaterslater@gmail.com>
2021-10-26 21:13:53 -0400

    adds maxSize rule

---
+++ schemas/post.js
@@ -0,0 +1,53 @@
+import { maxSize } from "../rules"
+
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'string',
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      validation: Rule => Rule.custom(async image => maxSize(image))
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    },
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const {author} = selection
+      return Object.assign({}, selection, {
+        subtitle: author && `by ${author}`,
+      })
+    },
+  },
+}
